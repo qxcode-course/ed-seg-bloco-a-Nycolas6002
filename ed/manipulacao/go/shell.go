18e03338879adc6a2d1177cc6b83796eee1f14fb
Nycolas6002 <nycolas1975@gmail.com>
2026-03-29 21:20:31 -0300

    fiz a atividade manipulação - Estressados Operações 3

--- ed/manipulacao/go/shell.go
+++ ed/manipulacao/go/shell.go
@@ -9,38 +9,150 @@ import (
 )
 
 func getMen(vet []int) []int {
-	_ = vet
-	return nil
+
+	var listaHomens []int
+	
+	for i :=0 ; i < len(vet); i++{
+		if vet[i] > 0 {
+			listaHomens = append(listaHomens, vet[i])
+		}
+	}
+
+	return listaHomens
+
 }
 
 func getCalmWomen(vet []int) []int {
-	_ = vet
-	return nil
+
+	var listaMulheresEstressadas []int
+
+	for i := 0; i < len(vet); i++{
+		if(vet[i] > -10 && vet[i] < 0){
+			listaMulheresEstressadas = append(listaMulheresEstressadas,vet[i])
+		}
+	}
+
+	return listaMulheresEstressadas
+
 }
 
 func sortVet(vet []int) []int {
-	_ = vet
-	return nil
+
+	listaOrdenada := make([]int, len(vet))
+	copy(listaOrdenada,vet)
+	n := len(listaOrdenada)
+
+	for i:= 0; i < n-1; i++{
+		for j:= 0; j < n-1-i; j++{
+			if(listaOrdenada[j] > listaOrdenada[j+1]){
+				temp := listaOrdenada[j]
+				listaOrdenada[j] = listaOrdenada[j+1]
+				listaOrdenada[j+1] = temp
+			}
+		}
+	}
+
+	return listaOrdenada
+
 }
 
 func sortStress(vet []int) []int {
-	_ = vet
-	return nil
+	
+	listaEstressados:= make([]int, len(vet))
+	copy(listaEstressados,vet)
+	n := len(listaEstressados)
+
+	 abs := func(x int) int{
+		if x < 0{
+			return -x
+		}
+
+		return x
+	}
+
+	for i:= 0; i < n-1; i++{
+		for j:= 0; j < n-1-i; j++{
+			if(abs(listaEstressados[j]) > abs(listaEstressados[j+1])){
+				temp := listaEstressados[j]
+				listaEstressados[j] = listaEstressados[j+1]
+				listaEstressados[j+1] = temp
+			}
+		}
+	}
+
+	return listaEstressados
+
 }
 
 func reverse(vet []int) []int {
-	_ = vet
-	return nil
+
+	var listaReversa []int
+
+	for i := len(vet) -1; i >= 0 ; i-- {
+		listaReversa = append(listaReversa, vet[i])
+	}
+
+	return listaReversa
+
 }
 
 func unique(vet []int) []int {
-	_ = vet
-	return nil
+
+	mapaUnicos := make(map[int]int, len(vet))
+	var listaUnicos[]int
+
+	for _,elementoVet := range vet{
+		_, existe := mapaUnicos[elementoVet]
+		if(!existe){
+			mapaUnicos[elementoVet] = 1
+		}else{
+			mapaUnicos[elementoVet]++
+		}
+	}
+
+	for chave,_ := range mapaUnicos{
+		listaUnicos = append(listaUnicos, chave)
+	}
+
+	return listaUnicos
+
 }
 
 func repeated(vet []int) []int {
-	_ = vet
-	return nil
+
+	mapaRepetidos := make(map[int]int, len(vet))
+	var listaRepetidos []int
+
+	for _,elementoVet := range vet{
+		_, existe := mapaRepetidos[elementoVet]
+		if(!existe){
+			mapaRepetidos[elementoVet] = 1
+		}else{
+			mapaRepetidos[elementoVet]++
+		}
+	}
+
+	for chave, valor := range mapaRepetidos{
+		if(valor >= 2){
+			for i :=0; i < valor-1 ;i++{
+				listaRepetidos = append(listaRepetidos, chave)
+			}
+		}
+	}
+
+	tamanhoLista := len(listaRepetidos)
+
+	for i:=0; i < tamanhoLista-1; i++{
+		for j :=0 ; j < tamanhoLista-1-i; j++ {
+			if(listaRepetidos[j] > listaRepetidos[j+1]){
+				temp := listaRepetidos[j]
+				listaRepetidos[j] = listaRepetidos[j+1]
+				listaRepetidos[j+1] = temp
+			}
+		}
+	}
+
+	return listaRepetidos
 }
 
 func main() {
