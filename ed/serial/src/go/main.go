7fe84c0ca312a34e9fc30e3fb001e1c0ef4ba29b
Nycolas6002 <nycolas1975@gmail.com>
2026-06-30 16:19:22 -0300

    fiz algumas questões de árvore foram elas: inverter,clonar, soma, altura, compare e serial. A questão do serial foi resolvida rápido pois antes de resolver essa questão eu resolvi outras questões que já tinha a função create.

--- ed/serial/src/go/main.go
+++ ed/serial/src/go/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -20,8 +21,17 @@ type Node struct {
 // Você pode fazer um "push_front" no array usando '*parts = (*parts)[1:]
 // Se o elemento for "#", significa que o nó é nulo.
 func create(parts *[]string) *Node {
-	_ = parts
-	return nil
+	elem := (*parts)[0]
+	*parts = (*parts)[1:]
+	if(elem == "#"){
+		return nil
+	}
+
+	value, _ := strconv.Atoi(elem)
+	node := &Node{Value: value}
+	node.Left = create(parts)
+	node.Right = create(parts)
+	return node
 }
 
 // BShow é uma função auxiliar para imprimir a árvore binária.
